5eb6f728a9501863b275caf08466e231247886e1
e.maslov <maslov.hr@gmail.com>
2025-04-04 20:00:36 +0200

    upd  main Gloria Machina Spiritus!

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import logging
 from flask import Flask, request
 from telegram import Update, Bot
-from telegram.ext import Dispatcher, CommandHandler, MessageHandler, filters
+from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Filters
 import openai
 import os
 import json
@@ -190,7 +190,7 @@ dispatcher.add_handler(CommandHandler("status", status))
 dispatcher.add_handler(CommandHandler("allow", allow))
 dispatcher.add_handler(CommandHandler("remove", remove))
 dispatcher.add_handler(CommandHandler("dump_whitelist", dump_whitelist))
-dispatcher.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_post))
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_post))
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
